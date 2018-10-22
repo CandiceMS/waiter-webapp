b589ab41f0569c1bb520a89417e8da5ab951ca3c
Candice <candice.m.sprout@gmail.com>
2018-10-22 14:47:38 +0200

    Tests in-progress

--- test/waiter_tests.js
+++ test/waiter_tests.js
@@ -31,9 +31,9 @@ describe('Add and display waiters, shifts and their relation', function(){
       let waiterAdd = await factoryLogic.storeInDB('Alice','monday');
         assert.equal(waiterAdd, " Thank you Alice. Your shifts have been saved");
     });
-    it('should add the registration number for Paarl', async function(){
+    it('should add the waiter name and all shifts selected', async function(){
         let shiftsAdd = await factoryLogic.storeInDB('Jack',['monday', 'tuesday', 'thursday']);
-          assert.deepEual(shiftsAdd, " Thank you Jack. Your shifts have been saved");
+          assert.deepEqual(shiftsAdd, " Thank you Jack. Your shifts have been saved");
     });
     it('should not add the shifts selected because no waiter name has been entered', async function(){
         let noAdd = await factoryLogic.storeInDB('', ['friday', 'saturday']);
@@ -56,29 +56,29 @@ describe('Add and display waiters, shifts and their relation', function(){
        assert.deepEqual([], await factoryLogic.getWaiterShifts(shiftsEntered));
     });
     
-    it('should return only registration numbers for the selected town', async function(){
-         await factoryLogic.storeInDB('Greg',['tuesday', 'saturday', 'sunday']);
-         await factoryLogic.storeInDB('Candice',['tuesday', 'saturday', 'sunday']);
-         await factoryLogic.storeInDB('Andrew',['monday', 'wednesday', 'friday']);
-         await factoryLogic.storeInDB('Nathri',['monday', 'wednesday', 'friday']);
-         await factoryLogic.storeInDB('Yegan',['tuesday', 'wednesday', 'saturday']);
-         
-        assert.deepEqual([{'reg_number': 'ca 670-901'}, {'reg_number': 'ca 687-901'}, {'reg_number': 'ca 680-000'}], await factoryRegNumbers.returnFilter('cape town'));
-    });
-     it('should clear all values in the reg_numbers table in the database', async function(){
-         await factoryLogic.storeInDB('CL 687-978','stellenbosch');
-         await factoryLogic.storeInDB('CA 687-945','cape town');
-         await factoryLogic.storeInDB('CA 680-363','cape town');
-         await factoryLogic.storeInDB('CY 679-589','bellville');
-        assert.deepEqual([], await factoryLogic.resetReg());
-     });
-     it('should clear all values in the towns table in the database', async function(){
-        await factoryLogic.storeInDB('CL 687-924','stellenbosch');
-        await factoryLogic.storeInDB('CA 687-879','cape town');
-        await factoryLogic.storeInDB('CA 680-112','cape town');
-        await factoryLogic.storeInDB('CY 679-252','bellville');
-       assert.deepEqual([], await factoryLogic.resetReg(), await factoryLogic.resetTowns());
-    });
+    // it('should return only registration numbers for the selected town', async function(){
+    //      await factoryLogic.storeInDB('Greg',['tuesday', 'saturday', 'sunday']);
+    //      await factoryLogic.storeInDB('Candice',['tuesday', 'saturday', 'sunday']);
+    //      await factoryLogic.storeInDB('Andrew',['monday', 'wednesday', 'friday']);
+    //      await factoryLogic.storeInDB('Nathri',['monday', 'wednesday', 'friday']);
+    //      await factoryLogic.storeInDB('Yegan',['tuesday', 'wednesday', 'saturday']);
+
+    //     assert.deepEqual([{'reg_number': 'ca 670-901'}, {'reg_number': 'ca 687-901'}, {'reg_number': 'ca 680-000'}], await factoryRegNumbers.returnFilter('cape town'));
+    // });
+    //  it('should clear all values in the reg_numbers table in the database', async function(){
+    //      await factoryLogic.storeInDB('CL 687-978','stellenbosch');
+    //      await factoryLogic.storeInDB('CA 687-945','cape town');
+    //      await factoryLogic.storeInDB('CA 680-363','cape town');
+    //      await factoryLogic.storeInDB('CY 679-589','bellville');
+    //     assert.deepEqual([], await factoryLogic.resetReg());
+    //  });
+    //  it('should clear all values in the towns table in the database', async function(){
+    //     await factoryLogic.storeInDB('CL 687-924','stellenbosch');
+    //     await factoryLogic.storeInDB('CA 687-879','cape town');
+    //     await factoryLogic.storeInDB('CA 680-112','cape town');
+    //     await factoryLogic.storeInDB('CY 679-252','bellville');
+    //    assert.deepEqual([], await factoryLogic.resetReg(), await factoryLogic.resetTowns());
+    // });
 
     after(async function() {
         await pool.end();
